06186239ebec7e0a6ced21b80d7239f7f2770860
Vladimir.Kuzmin <Vladimir.Kuzmin@effective-soft.com>
2015-05-28 17:56:03 +0300

    hotfix

--- format-decimals.js
+++ format-decimals.js
@@ -3,16 +3,17 @@ angular.module('formatDecimal', [])
         return function(value) {
             var abbr = ['k', 'M', 'B', 'T'],
                 i = abbr.length - 1,
-                result;
+                result,
+				decimal;
 
             for (i; i>=0; i--) {
-                var size = Math.pow(10, (i+1)*3),
-                    decimal = String(value/size).split('.')[1].substr(0, 1);
+                var size = Math.pow(10, (i+1)*3);
 
                 if (size <= value) {
                     if (value % size < 100) {
                         result = (value/size).toFixed(0) + abbr[i];
                     } else {
+						decimal = String(value/size).split('.')[1].substr(0, 1);
                         result = (value/size).toFixed(0) + '.' + decimal + abbr[i];
                     }
                     break;
